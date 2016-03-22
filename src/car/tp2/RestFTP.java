1c6338aa1bca9130d8355551151bd57742c532e3
Badreddine Ilhem <badreddine@a12p5.fil.univ-lille1.fr>
2016-03-22 18:52:13 +0100

    login

--- src/car/tp2/RestFTP.java
+++ src/car/tp2/RestFTP.java
@@ -1,17 +1,17 @@
 package car.tp2;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.commons.net.ftp.FTPClient;
@@ -20,6 +20,9 @@ import org.apache.commons.net.ftp.FTPFile;
 @Path("/restftp")
 public class RestFTP {
 
+	String userName = "";
+	String passWord = "";
+	
 	// Pour tester:
 	// curl http://localhost:8080/rest/api/helloworld/getfile
 	@GET
@@ -30,7 +33,7 @@ public class RestFTP {
 		final FTPClient ftpClient = new FTPClient();
 		try {
 			ftpClient.connect("edel-brau.lifl.fr", 21);
-			final boolean login = ftpClient.login("anonymous", "bob");
+			final boolean login = ftpClient.login(userName, passWord);
 
 			if (login) {
 				final InputStream is = ftpClient.retrieveFileStream(file);
@@ -48,6 +51,8 @@ public class RestFTP {
 						}
 					};
 				}
+			} else {
+				System.out.println("erreur to be implemented");
 			}
 
 		} catch (IOException e) {
@@ -58,7 +63,8 @@ public class RestFTP {
 	}
 
 	// Pour tester:
-	// curl -H "Content-Type: application/octet-stream" -X POST -d "tralala" http://localhost:8080/rest/api/helloworld/postfile
+	// curl -H "Content-Type: application/octet-stream" -X POST -d "tralala"
+	// http://localhost:8080/rest/api/helloworld/postfile
 	@POST
 	@Path("/postfile/{var: .*}")
 	@Consumes("application/octet-stream")
@@ -67,12 +73,12 @@ public class RestFTP {
 		final FTPClient ftpClient = new FTPClient();
 		try {
 			ftpClient.connect("edel-brau.lifl.fr", 21);
-			final boolean login = ftpClient.login("anonymous", "bob");
+			final boolean login = ftpClient.login(userName, passWord);
 
 			if (login) {
-				if(ftpClient.storeFile(file, is))
+				if (ftpClient.storeFile(file, is))
 					return ("Envoi fichier OK (POST)\n");
-				
+
 				ftpClient.logout();
 			}
 		} catch (IOException e) {
@@ -90,26 +96,26 @@ public class RestFTP {
 		final FTPClient ftpClient = new FTPClient();
 		try {
 			ftpClient.connect("edel-brau.lifl.fr", 21);
-			final boolean login = ftpClient.login("anonymous", "bob");
+			final boolean login = ftpClient.login(userName, passWord);
 
 			if (login) {
-				if(file.equals("home")){
+				if (file.equals("home")) {
 					FTPFile files[] = ftpClient.listFiles();
 					String nameFiles = "";
-					for(FTPFile f : files){
+					for (FTPFile f : files) {
 						nameFiles += f.getName() + "</br>";
 					}
 					ftpClient.logout();
 					return nameFiles;
-				}else if (ftpClient.changeWorkingDirectory(file)){
+				} else if (ftpClient.changeWorkingDirectory(file)) {
 					FTPFile files[] = ftpClient.listFiles();
 					String nameFiles = "";
-					for(FTPFile f : files){
+					for (FTPFile f : files) {
 						nameFiles += f.getName() + "</br>";
 					}
 					ftpClient.logout();
 					return nameFiles;
-				}else{
+				} else {
 					return ("Incorrect path </br>");
 				}
 			}
@@ -118,7 +124,7 @@ public class RestFTP {
 		}
 		return ("Incorrect path </br>");
 	}
-	
+
 	@GET
 	@Path("/deletefile/{var: .*}")
 	@Produces("text/html")
@@ -127,19 +133,39 @@ public class RestFTP {
 		final FTPClient ftpClient = new FTPClient();
 		try {
 			ftpClient.connect("edel-brau.lifl.fr", 21);
-			final boolean login = ftpClient.login("anonymous", "bob");
+			final boolean login = ftpClient.login(userName, passWord);
 
 			if (login) {
-				if(ftpClient.deleteFile("upload/"+file)){
+				if (ftpClient.deleteFile("upload/" + file)) {
 					ftpClient.logout();
-					return ("File "+file+" deleted </br>");
-				}else{
+					return ("File " + file + " deleted </br>");
+				} else {
 					return ("Incorrect path </br>");
 				}
-			}
+			} 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return ("Incorrect path </br>");
+	}
+
+
+	@GET
+	@Produces("text/html")
+	public String loginForm() {
+		return " <form method=\"post\">"
+				+ "name:<br>"
+				+ "<input type=\"text\" name=\"name\" value=\"Mickey\"><br>"
+				+ "Password:<br>"
+				+ "<input type=\"text\" name=\"pass\" value=\"Mouse\"><br><br>"
+				+ "<input type=\"submit\" value=\"Submit\">" + "</form> ";
+	}
+
+	@POST
+	@Consumes("application/x-www-form-urlencoded")
+	public Response post(@FormParam("name") String name, @FormParam("pass") String pass) {
+		userName = name;
+		passWord = pass;
+		return Response.ok("connect ok").build();
 	}
 }
